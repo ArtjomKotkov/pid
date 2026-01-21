611104bc0c400111ca05ee85ca0dc56bfc4e58ae
akotkov <akotkov@webim.ru>
2026-01-21 19:56:31 +0700

    Update setup.py to bump version to 1.0.0 and require Python >=3.13

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@ from setuptools import setup, find_packages
 
 setup(
     name='pid',
-    version='0.2.0',
+    version='1.0.0',
     packages=find_packages(),
     tests_require=[
         'pytest==7.3.1',
     ],
-    python_requires='>=3.11'
+    python_requires='>=3.13'
 )
